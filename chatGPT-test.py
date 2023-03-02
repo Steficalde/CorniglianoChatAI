47576b91f3f30d2ce2996267fb6a3972291f4068
SignoCreator <signosignodiego@gmail.com>
2023-03-02 13:29:19 +0100

    chatgpiccola

--- chatGPT-test.py
+++ chatGPT-test.py
@@ -2,22 +2,30 @@ import os
 import openai
 from colorama import Fore, Style
 
-new_api_key = "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"
+new_api_key = "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"
+
+prompts = [
+    "Domanda: Come mi chiamo?\nRisposta: Tu ti chiami Diego.",
+    "Domanda: Qual è il mio nome?\nRisposta: Il tuo nome è Diego.",
+    "Domanda: Mi puoi dire il mio nome?\nRisposta: Certamente, tu ti chiami Diego."
+]
 
 
+# Use the trained model to chat
+os.system('cls')
 def chat(prompt):
     completions = openai.Completion.create(model="text-davinci-003", prompt=prompt, max_tokens=1024,
                                            api_key=new_api_key)
-    message = completions[0].text
+    message = completions.choices[0].text
     return message
 
 
 os.system('cls')
 i = 1
 while i < 6:
-    human = input(Fore.YELLOW + Style.BRIGHT)
+    human = input(Fore.YELLOW + Style.BRIGHT+'<human>')
     robot = chat(human)
-    print(Fore.GREEN + Style.BRIGHT + '<little robot>' + robot)
+    print(Fore.GREEN + Style.BRIGHT + '<ROBerto>' + robot)
     i = i + 1
 print('')
 print(Fore.RED + Style.BRIGHT + 'End of the conversation')
